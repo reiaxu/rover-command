db8896792d1e25f3c49fe08de3cde87ec1cde9b7
reiaxu <59967035+reiaxu@users.noreply.github.com>
2021-05-29 04:00:22 +0100

    added some mqtt stuff but doesn't connect to websocket - idk why

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, Component } from 'react';
 import './App.css';
 import ToggleSwitch from './toggle-switch/toggle-switch';
 import {
@@ -8,93 +8,67 @@ import {
   Circle,
   Rectangle
 } from 'draw-shape-reactjs';
-import mqttService from "./mqtt/mqtt";
+
+import { Connector } from "mqtt-react";
+import PostMqtt from './postMessage.js';
 
 import up from './images/up.png';
 import down from './images/down.png';
 import left from './images/left.png';
 import right from './images/right.png';
 
-// var mqtt    = require('mqtt');
-// var options = {
-// 	protocol: 'mqtts',
-// 	// clientId uniquely identifies client
-// 	// choose any string you wish
-// 	clientId: 'marsrover' 	
-// };
-// var client  = mqtt.connect('mqtt://test.mosquitto.org:8081', options);
-// client.subscribe('topic'); // topic
+class App extends Component {
+  // function moveup() {
+  //   console.log('1');
+  // }
 
+  // function movedown() {
+  //   console.log('2');
+  // }
 
-
-function WelcomeMessage() {
-  return <p>Welcome!</p>
-}
-
-function App() {
-  function moveup() {
-    console.log('1');
-  }
-
-  function movedown() {
-    console.log('2');
-  }
-
-  function moveleft() {
-    console.log('3');
-  }
+  // function moveleft() {
+  //   console.log('3');
+  // }
   
-  function moveright() {
-    console.log('4');
+  // function moveright() {
+  //   console.log('4');
+  // }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-lhs">
+          <header className="App-map">
+
+          </header>
+
+        </div>
+
+        <div className="App-sidebar">
+
+          <header className="App-description">
+    
+          </header>
+
+          <header className="App-warnings">
+            <Circle center={[1004, 294]} radius={10} color='#BADA55' />
+            <p>Your rover is safe! No potential collisions detected.</p>
+          </header>
+
+          <ToggleSwitch Name='toggle' /> <p className="toggle-description">Enable autonomous mode</p>
+          {/* (we could add stuff like move towards x-coloured ball etc) */}
+        
+        
+          <header className="App-buttons">
+            <PostMqtt />
+          </header>
+        </div>
+      </div>
+    );
   }
-
-  // var note;
-  // client.on('message', function (topic, message) {
-  //   note = message.toString();
-  //   // Updates React state with message 
-  //   setMesg(note);
-  //   console.log(note);
-  //   client.end();
-  //   });
-
-  // // Sets default React state 
-  // const [mesg, setMesg] = useState(<Fragment><em>nothing heard</em></Fragment>);
-
-  return (
-    <div className="App">
-      <div className="App-lhs">
-        <header className="App-map">
-
-        </header>
-
-      </div>
-
-      <div className="App-sidebar">
-
-        <header className="App-description">
-        {/* <p>The message is: {mesg}</p> */}
-	
-        </header>
-
-        <header className="App-warnings">
-          <Circle center={[1004, 294]} radius={10} color='#BADA55' />
-          <p>Your rover is safe! No potential collisions detected.</p>
-        </header>
-
-        <ToggleSwitch Name='toggle' /> <p className="toggle-description">Enable autonomous mode</p>
-        {/* (we could add stuff like move towards x-coloured ball etc) */}
-      
-      
-        <header className="App-buttons">
-          <img src={up} className="App-logo" alt="up-button" onClick={moveup} /><br></br>
-          <img src={left} className="App-logo" alt="left-button" onClick={moveleft} />
-          <img src={down} className="App-logo" alt="down-button" onClick={movedown} />
-          <img src={right} className="App-logo" alt="right-button" onClick={moveright} />
-
-        </header>
-      </div>
-    </div>
-  );
 }
 
-export default App;
+export default () => (
+  <Connector mqttProps="wss://test.mosquitto.org">
+      <App />
+  </Connector>
+);
